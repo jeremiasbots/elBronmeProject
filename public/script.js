88c569e2c1d4f9e691bb2ef1f25c49acea35e736
jeremiasbots <victorrbravo@icloud.com>
2025-01-29 17:58:50 -0500

    Añadiendo preguntas acertadas por alumno y sistema para responder con "A, B, C, D, E"

--- public/script.js
+++ public/script.js
@@ -1,19 +1,37 @@
 const cantAlumnos = Number(prompt("¿Cuántos alumnos son?"));
 const preguntasExamen = prompt("¿De cuántas preguntas es el examen?");
-const respuestaAfirmativa = "s";
 let acertadas = 0;
 let totalAcertadas = 0;
+const questionsWithAnswers = [];
+
+function createQuestions() {
+  for (let p = 0; p < preguntasExamen; p++) {
+    const correctAnswer = prompt(
+      `¿Cuál es la respuesta correcta de la pregunta ${p + 1} en formato A, B, C, D, E?`
+    );
+    const options = ["a", "b", "c", "d", "e"];
+    if (!options.some((o) => o === correctAnswer.toLowerCase())) {
+      alert("Respuesta con formato no válido, vuelva a recargar la página");
+      return;
+    }
+    questionsWithAnswers.push({ questionIndex: p, correctAnswer });
+  }
+}
+createQuestions();
 
 for (let i = cantAlumnos; i > 0; i--) {
   const nombre = prompt("¿Cuál es el nombre del alumno?");
   for (let m = 1; preguntasExamen >= m; m++) {
-    const pregunta = prompt(`La pregunta ${m} es correcta`);
-    if (pregunta === respuestaAfirmativa) acertadas++;
+    const pregunta = prompt(`Opción (A, B, C, D, E) de la pregunta ${m}`);
+    const questionSome = questionsWithAnswers.find((x) => x.questionIndex === (m - 1));
+    if (questionSome && pregunta === questionSome.correctAnswer) {
+      acertadas++;
+    }
   }
   const nota = (20 / parseInt(preguntasExamen)) * acertadas;
   const pElement = document.createElement("p");
   pElement.id = `estudiante${i}`;
-  pElement.innerHTML = `${nombre}: ${nota}<br/>`;
+  pElement.innerHTML = `${nombre}: ${nota} (${acertadas}/${preguntasExamen})<br/>`;
   document.body.appendChild(pElement);
   totalAcertadas += acertadas;
   acertadas = 0;
